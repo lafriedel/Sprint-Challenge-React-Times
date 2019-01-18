c3e2008135853f646c7e9c5c2e1c0dbaa05f7e41
Leigh-Ann Friedel <leigh.ann.friedel@gmail.com>
2019-01-18 11:33:48 -0500

    Build out and add PropTypes for Cards

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -4,12 +4,16 @@ import Card from './Card';
 const Cards = props => {
   return (
     <div className="cards-container">
+      {props.cards.map(card => <Card card={card} />)}
       {/* Using the cards prop, map over the list creating a 
           new Card component for each passing the card as the only prop*/}
     </div>
   )
 }
 
+Cards.propTypes = {
+  cards: PropTypes.func
+}
 // Make sure you include prop types for all of your incoming props
 
 export default Cards;

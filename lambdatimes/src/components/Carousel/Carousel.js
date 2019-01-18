5ccb8c5a7991c2e0ff39b142c33982af327445ec
Leigh-Ann Friedel <leigh.ann.friedel@gmail.com>
2019-01-18 12:56:31 -0500

    Render Carousel, build state, implement rightClick func

--- lambdatimes/src/components/Carousel/Carousel.js
+++ lambdatimes/src/components/Carousel/Carousel.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
 import { carouselData } from '../../data'
+
 // Complete this Carousel 
 export default class Carousel extends Component {
   constructor(props){
     super(props);
     this.state = {
-
+      images: [],
+      index: 0,
+      displayedImg: ""
     }
   }
   componentDidMount(){
-
+    this.setState({
+      images: carouselData,
+      displayedImg: carouselData[0]
+    })
   }
 
   leftClick = () => {
@@ -17,17 +23,27 @@ export default class Carousel extends Component {
   }
 
   rightClick = () => {
+    if (this.state.index < 3) {
+      this.setState(prevState => ({index: ++this.state.index, displayedImg: carouselData[this.state.index]}))
+    } else {
+      this.setState({
+        index: 0,
+        displayedImg: carouselData[0]
+      })
+    }
 
   }
 
   selectedImage = () => {
-    return <img src={} style={{display: 'block'}} />
+    return <img src={this.state.displayedImg} style={{display: 'block'}} />
   }
   
   render(){
+    console.log(this.state.displayedImg);
     return (
       <div className="carousel">
         <div className="left-button" onClick={this.leftClick}>{"<"}</div>
+        {this.selectedImage()}
         <div className="right-button" onClick={this.rightClick}>{">"}</div>
       </div>
     )

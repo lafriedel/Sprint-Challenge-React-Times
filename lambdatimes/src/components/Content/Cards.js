e74c5fdc67fdc9ac2d1ea9d0d72bf1de28a2f500
Leigh-Ann Friedel <leigh.ann.friedel@gmail.com>
2019-01-18 11:38:08 -0500

    Build out Card and add PropTypes, update Cards PropTypes

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import Card from './Card';
+import PropTypes from 'prop-types';
 
 const Cards = props => {
   return (
     <div className="cards-container">
-      {props.cards.map(card => <Card card={card} />)}
+      {props.cards.map(card => <Card card={card} key={Math.random()} />)}
       {/* Using the cards prop, map over the list creating a 
           new Card component for each passing the card as the only prop*/}
     </div>
@@ -12,7 +13,7 @@ const Cards = props => {
 }
 
 Cards.propTypes = {
-  cards: PropTypes.func
+  cards: PropTypes.array
 }
 // Make sure you include prop types for all of your incoming props
 
